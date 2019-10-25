70d48e9236dd24e83aa6d1d2e8339af506fb693a
abdullahcangul <abdullahcangul@gmail.com>
2019-10-25 16:58:57 +0300

    importlar ve önyüz eklendi

--- src/main/java/com/cngl/bilet/DemoApplication.java
+++ src/main/java/com/cngl/bilet/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.cngl.bilet;
 
+import java.util.Arrays;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class DemoApplication {
